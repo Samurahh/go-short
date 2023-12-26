689e233f00e3913cfb6078e3c25376e4fd4e620f
Andrei Pavel <samurahh@gmail.com>
2023-12-26 20:54:48 +0000

    Explored a basic http server

--- go-short.go
+++ go-short.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
-	panic("not implemented")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		timeBytes, _ := time.Now().MarshalText()
+		fmt.Fprintf(w, "Hello, world! Time is %v\n", html.EscapeString(string(timeBytes)))
+	})
+
+	s := &http.Server{
+		Addr: ":6090",
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
