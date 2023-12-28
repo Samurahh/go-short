3a1996725ee1b5488701e8018a02059c898a4496
Kevin Nielsen <keveran@gmail.com>
2023-12-28 22:08:53 +0100

    [Feature] Add the option to change the address and port the http server is listening on based on the cmd arguments (#5)
    * feat: add -p cli argument to specify http port
    
    * feat: add cmd parsing for flag -h for hostname
    
    * feat: add long flag --port and change host to address
    
    * refactor: remove unnecessary strconv
    
    * feat: panic if port out of range
    
    * refactor: change panicln to Panicf
    
    * refactor: change %s to correct %v for int

--- go-short.go
+++ go-short.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -8,10 +10,22 @@ import (
 )
 
 var httpServer *http.Server = &http.Server{
-	Addr: ":8080", // default port
+	Addr:    ":8080", // default port
 	Handler: router.Router(),
 }
 
 func main() {
+	var port int
+	var address string
+	flag.IntVar(&port, "p", 8080, "port to run the http server on | default 8080")
+	flag.IntVar(&port, "port", 8080, "port to run the http server on | default 8080")
+	flag.StringVar(&address, "address", "", "address the http server will run on | default 127.0.0.1 / localhost")
+	flag.Parse()
+
+	if port < 0 || port > 65535 {
+		log.Panicf("Port %v out of range [0, 65535]", port)
+	}
+	httpServer.Addr = fmt.Sprintf("%s:%v", address, port)
+
 	log.Fatal(httpServer.ListenAndServe())
 }
