9e2875199235097fa4755510acfd626238c78f5b
kevinanielsen <keveran@gmail.com>
2023-12-28 21:11:10 +0100

    feat: add -p cli argument to specify http port

--- go-short.go
+++ go-short.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Samurahh/go-short/router"
 )
 
 var httpServer *http.Server = &http.Server{
-	Addr: ":8080", // default port
+	Addr:    ":8080", // default port
 	Handler: router.Router(),
 }
 
 func main() {
+	var port int
+	flag.IntVar(&port, "p", 8080, "port to run the server on")
+	flag.Parse()
+
+	if port != 0 {
+		portStr := strconv.Itoa(port)
+		httpServer.Addr = ":" + portStr
+	}
+
 	log.Fatal(httpServer.ListenAndServe())
 }
