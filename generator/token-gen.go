a31553856a96dc382a1397445c0637c7f67f28d7
kevinanielsen <keveran@gmail.com>
2023-12-30 09:09:55 +0100

    fix: token generator with byte secretkey

--- generator/token-gen.go
+++ generator/token-gen.go
@@ -2,13 +2,15 @@ package generator
 
 import "github.com/golang-jwt/jwt"
 
+var secretKey = []byte("SECRET")
+
 func GenerateToken(url string) (string, error) {
 	var claims = jwt.MapClaims{
 		"url": url,
 	}
 
-	jwtToken := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
-	signed, err := jwtToken.SignedString("SECRET")
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	signed, err := jwtToken.SignedString(secretKey)
 	if err != nil {
 		return "", err
 	}
